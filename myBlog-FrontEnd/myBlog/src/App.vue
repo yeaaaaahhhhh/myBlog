66d566e59ef87b78821b0147b8afdf2d747f7d04
yehyeon <qkdo12@naver.com>
2023-12-09 22:44:21 +0900

    :sparkles:Fix : vue 불필요한 파일 삭제
    이슈 번호: #4
    
    - vue 기본 프로젝트에서 불필요한 파일 삭제

--- myBlog-FrontEnd/myBlog/src/App.vue
+++ myBlog-FrontEnd/myBlog/src/App.vue
@@ -1,23 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    basic template
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
