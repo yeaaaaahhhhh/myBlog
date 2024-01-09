8b8fffafa32e0ff1259867353ca76dbfa48c1645
yeaaaaahhhhh <k210029@naver.com>
2024-01-09 20:44:13 +0900

    :sparkles: 백엔드 프로젝트 생성
    이슈 번호: #11

---
+++ myBlog-BackEnd/src/test/java/com/hazel/myblogbackend/MyBlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazel.myblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
